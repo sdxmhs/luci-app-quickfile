89b9e3e60e2e3527a8bb067cc4716ed827908027
sdxmhs <huchd0@gmail.com>
2026-05-19 21:17:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# 使用 feeds 添加源，这种方式由 OpenWrt 编译系统统一管理，会自动处理认证
-# 这样就不会出现 "could not read Username" 的权限错误了
+# 只向 feeds.conf.default 追加内容
 echo 'src-git quickfile https://github.com/sbwml/luci-app-quickfile' >> feeds.conf.default
 echo 'src-git quickfile_core https://github.com/sbwml/quickfile' >> feeds.conf.default
-
-# 更新 feeds (会自动下载上面定义的地址)
-./scripts/feeds update -a
-# 安装 feeds
-./scripts/feeds install -a
